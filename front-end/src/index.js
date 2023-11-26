5f4dd4c482a1a1465ac52852e2013c2a95970e50
Arthur L <18040415+alouradou@users.noreply.github.com>
2023-11-26 20:27:33 +0100

    Displaying maps

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,20 +1,56 @@
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
-const mymap = L.map('map').setView([51.505, -0.09], 13);
+// Coordonnées du centre de votre carte
+const center = [0, 0];
 
-const customIcon = L.icon({
-    iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-    iconSize: [25, 41], // Taille de l'icône [largeur, hauteur]
-    iconAnchor: [12, 41], // Point d'ancrage de l'icône par rapport à sa position
-    popupAnchor: [1, -34] // Point d'ancrage du popup par rapport à la position du marqueur
+// Taille fixe de l'image (en degrés)
+const imageSize = 0.5;
+
+// Calcul des coordonnées du coin supérieur gauche et inférieur droit de l'image
+const imageBounds = [
+    [center[0] - imageSize / 2, center[1] - imageSize / 2], // Coin supérieur gauche
+    [center[0] + imageSize / 2, center[1] + imageSize / 2]  // Coin inférieur droit
+];
+
+// Créez une carte avec le fond d'image
+const mymap = L.map('map').setView(center, 13);
+
+// Ajoutez l'image en tant que fond de carte
+const imageUrl = './assets/region2-00250-01-01-detailed.bmp'; // Remplacez par le chemin correct de votre image
+L.imageOverlay(imageUrl, imageBounds).addTo(mymap);
+
+
+// Ajoutez des réflexions dans toutes les directions
+const reflections = [
+    { dx: 0, dy: imageSize },  // Réflexion vers le bas
+    { dx: imageSize, dy: 0 },  // Réflexion vers la droite
+    { dx: 0, dy: -imageSize }, // Réflexion vers le haut
+    { dx: -imageSize, dy: 0 }, // Réflexion vers la gauche
+    { dx: imageSize, dy: imageSize },  // Réflexion vers le bas et la droite
+    { dx: imageSize, dy: -imageSize }, // Réflexion vers le haut et la droite
+    { dx: -imageSize, dy: -imageSize }, // Réflexion vers le haut et la gauche
+    { dx: -imageSize, dy: imageSize }  // Réflexion vers le bas et la gauche
+];
+
+// Ajoutez les réflexions
+reflections.forEach(reflection => {
+    const reflectedBounds = [
+        [center[0] - imageSize / 2 + reflection.dy, center[1] - imageSize / 2 + reflection.dx],
+        [center[0] + imageSize / 2 + reflection.dy, center[1] + imageSize / 2 + reflection.dx]
+    ];
+    L.imageOverlay(imageUrl, reflectedBounds).addTo(mymap);
 });
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(mymap);
 
+// Ajoutez un marqueur avec une icône personnalisée
+const customIcon = L.icon({
+    iconUrl: './assets/marker-icon.svg',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34]
+});
 
-L.marker([51.505, -0.09],{ icon: customIcon }).addTo(mymap)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+L.marker(center, { icon: customIcon }).addTo(mymap)
+    .bindPopup('<h3>Bienvenue aventurier !</h3><p>Tu es au centre de la carte.</p>')
     .openPopup();
