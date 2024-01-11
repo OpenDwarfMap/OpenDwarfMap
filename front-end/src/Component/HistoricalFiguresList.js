d5764d693689561a2a279526859e1c763a3ef676
adiouf <h8799r@gmail.com>
2024-01-12 00:14:37 +0100

    Page personnages historiques terminées

--- front-end/src/Component/HistoricalFiguresList.js
+++ front-end/src/Component/HistoricalFiguresList.js
@@ -11,16 +11,23 @@ function HistoricalFiguresList () {
     }, [pagination])
     return (<div>
         <div className={"hf-page-title"}>Personnages historiques</div>
+        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+            {parseInt(pagination) > 1 ?
+                <Link to={"/historical_figures/page/" + (parseInt(pagination) - 1).toString()} className={"hf-page-nav-btn"}> Page
+                    Précédente </Link> : <div></div>}
+            <div style={{color: "white"}}>page {pagination}</div>
+            <Link to={"/historical_figures/page/" + (parseInt(pagination) + 1).toString()} className={"hf-page-nav-btn"}> Page Suivante </Link>
+        </div>
         <ul>
             {HistoricalFiguresList.map((hf) => {
-                if(hf == null) return;
+                if (hf == null) return;
                 return (
-                    <Link to={"/historical_figure/"+hf.id.toString()}><li key={hf.id}> <HistoricalFigureListItem historicalFigure={hf}/></li></Link>
+                    <Link to={"/historical_figure/" + hf.id.toString()}>
+                        <li key={hf.id}><HistoricalFigureListItem historicalFigure={hf}/></li>
+                    </Link>
                 )
             })}
         </ul>
-        {parseInt(pagination) >1 ? <Link to={"/historical_figures/page/"+(parseInt(pagination)-1).toString()}> Page Précédente </Link>: null}
-        <Link to={"/historical_figures/page/"+(parseInt(pagination)+1).toString()}> Page Suivante </Link>
     </div>)
 
 }
