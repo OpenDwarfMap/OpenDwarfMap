69791c85ef44aa7e3dfa904180237ce117838660
Hagarde <43098624+Hagarde@users.noreply.github.com>
2024-02-20 16:39:19 +0100

    front éclaté qu sol pour entity page mais entity page ok fonctionnelle

--- front-end/src/Component/EntityDetail.js
+++ front-end/src/Component/EntityDetail.js
@@ -1,15 +1,37 @@
 import React, { useEffect, useState } from "react";
-import {getCategoryData} from '../utils/API.js';
+import {getCategoryDataDetail} from '../utils/API.js';
 import { useParams, Link } from "react-router-dom";
+import LoadingPage from "./LoadingPage.js";
+
 
 function EventDetail () {
     const { id } = useParams()
     const [EntityDetail, setEntityDetail] = useState(false)  
     useEffect(()=> {
-        getCategoryData(setEntityDetail, "entity", id);
+        getCategoryDataDetail(setEntityDetail, "entity", id);
     }, []);
+    console.log(EntityDetail);
 
-    return null;
+    return EntityDetail ?
+    (<div>
+        <div className={"hf-page-title"}> Entité : {EntityDetail.name}</div>
+        <div> Cette oragnisation a {EntityDetail.child} membres et est un {EntityDetail.type} </div>
+        {EntityDetail.race ? <div> Les individus qui appartiennent à cette organisation sont des {EntityDetail.race} </div> : null }
+        {EntityDetail.histfig_id  ? 
+        EntityDetail.histfig_id.map((hf)=>{
+            console.log(typeof hf)
+            return (
+                <div>
+                    <Link key={hf[0]} to={'/historical_figure/'+hf[0].toString()}>
+                        {hf[1]} { hf[2] ? (<div> qui occupe actuellement la position de {hf[2]} </div>) : null}
+                    </Link>
+                </div>
+            )
+        })
+        : null }
+    </div>)
+    : <LoadingPage /> ;
+        
 }
 
 export default EventDetail;
--- front-end/src/Component/LoadingPage.js
+++ front-end/src/Component/LoadingPage.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Spinner from 'react-bootstrap/Spinner';
 
 
 function LoadingPage () {
     return (
-        <Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
+        <div>
+          LOADING ...
+        </div>
       );
 }
 
