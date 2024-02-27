be4f9f913a4503298003ba925fcb0b11d5fd6acb
Hagarde <43098624+Hagarde@users.noreply.github.com>
2024-02-27 15:45:45 +0100

    caca

--- back-end-statique/newXmlToJson.js
+++ back-end-statique/newXmlToJson.js
@@ -1,7 +1,7 @@
 import { XMLParser, XMLBuilder, XMLValidator} from "fast-xml-parser"
 import fs from "fs"
 
-const filePath = '../../region2-00250-01-01-legends_plus.xml';
+const filePath = '../legends-region2-00250-01-01/region2-00250-01-01-legends_plus.xml';
 
 fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
@@ -18,11 +18,12 @@ fs.readFile(filePath, 'utf8', (err, data) => {
     };
     const parser = new XMLParser();
     let jObj = parser.parse(data);
-    fs.writeFile("legend_plus.json", JSON.stringify(jObj), (err) => {
+    const exportName = filePath.includes("plus") ? "legend_plus.json" : "legend.json" ;
+    fs.writeFile(exportName, JSON.stringify(jObj), (err) => {
         if (err) {
             console.error('Erreur lors de l\'écriture du fichier :', err);
         } else {
-            console.log('Le résultat a été écrit dans le fichier :', "legend_plus.json");
+            console.log('Le résultat a été écrit dans le fichier :', exportName);
         }
     });
 });
